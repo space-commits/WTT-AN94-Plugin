cd37dde9045c58acbdaafc357e4adb69936b0715
spacecommits <57108281+space-commits@users.noreply.github.com>
2025-01-30 13:24:47 +0000

    initial commit

---
+++ Patches.cs
@@ -0,0 +1,118 @@
+﻿using SPT.Reflection.Patching;
+using SPT.Reflection.Utils;
+using EFT.UI.Settings;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using EFT.Animations.NewRecoil;
+using EFT.Animations;
+using EFT;
+using HarmonyLib;
+using static EFT.Player;
+using static EFT.ScenesPreset;
+
+namespace WTTAN94
+{
+    public class UpdateWeaponVariablesPatch : ModulePatch
+    {
+        private static FieldInfo playerField;
+        private static FieldInfo fcField;
+
+        protected override MethodBase GetTargetMethod()
+        {
+            playerField = AccessTools.Field(typeof(FirearmController), "_player");
+            fcField = AccessTools.Field(typeof(ProceduralWeaponAnimation), "_firearmController");
+            return typeof(EFT.Animations.ProceduralWeaponAnimation).GetMethod("UpdateWeaponVariables", BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(EFT.Animations.ProceduralWeaponAnimation __instance)
+        {
+            FirearmController fc = (FirearmController)fcField.GetValue(__instance);
+            if (fc == null) return;
+            Player player = (Player)playerField.GetValue(fc);
+            if (player != null && player.IsYourPlayer && player.MovementContext.CurrentState.Name != EPlayerState.Stationary)
+            {
+                Plugin.IsAN94 = Plugin.GunIDs.Contains(fc.Weapon.TemplateId.ToString());         
+            }
+        }
+    }
+
+    public class ShootPatch : ModulePatch
+    {
+        private static FieldInfo _playerField;
+        private static FieldInfo _fcField;
+
+        protected override MethodBase GetTargetMethod()
+        {
+            _playerField = AccessTools.Field(typeof(FirearmController), "_player");
+            _fcField = AccessTools.Field(typeof(ProceduralWeaponAnimation), "_firearmController");
+            return typeof(ProceduralWeaponAnimation).GetMethod("Shoot", BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        [PatchPrefix]
+        private static void PatchPrefix(ProceduralWeaponAnimation __instance, ref float str)
+        {
+            FirearmController fc = (FirearmController)_fcField.GetValue(__instance);
+            if (fc == null) return;
+            Player player = (Player)_playerField.GetValue(fc);
+            if (player != null && player.IsYourPlayer && Plugin.IsAN94 && player.MovementContext.CurrentState.Name != EPlayerState.Stationary)
+            {
+                Plugin.IsFiring = true;
+                Plugin.ShotCount += 1;
+                str *= fc.Item.SelectedFireMode != EFT.InventoryLogic.Weapon.EFireMode.semiauto && Plugin.ShotCount <= 2 ? Plugin.BurstRecoilMulti.Value : 1f;
+            }
+        }
+    }
+
+    public class PlayerUpdatePatch : ModulePatch
+    {
+        private static float _shotTimer = 0f;
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(Player).GetMethod("LateUpdate", BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        private static void PWAUpdate(Player player, Player.FirearmController fc)
+        {
+            if (fc != null && Plugin.IsAN94)
+            {
+                //hyperburst
+                fc.Item.MalfState.OverheatFirerateMultInited = true;
+                if (Plugin.ShotCount <= 2 && fc.Item.SelectedFireMode != EFT.InventoryLogic.Weapon.EFireMode.semiauto)
+                {
+                    fc.Item.MalfState.OverheatFirerateMult = Plugin.BurstROFMulti.Value;
+                }
+                else fc.Item.MalfState.OverheatFirerateMult = 1f;
+            }
+
+            if (Plugin.IsFiring) 
+            {
+                _shotTimer += Time.deltaTime;
+
+                if (_shotTimer >= Plugin.ShotResetDelay.Value)
+                {
+                    _shotTimer = 0f;
+                    Plugin.IsFiring = false;
+                    Plugin.ShotCount = 0;
+                }
+            }
+
+            Logger.LogWarning($"Is Firing: {Plugin.IsFiring}, Shot Count: {Plugin.ShotCount}, Is AN94:  {Plugin.IsAN94}" );
+ 
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(Player __instance)
+        {
+            if (__instance.IsYourPlayer)
+            {
+                Player.FirearmController fc = __instance.HandsController as Player.FirearmController;
+                PWAUpdate(__instance, fc);
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Configuration;
+using Comfort.Common;
+using EFT;
+using SPT.Common.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using static GClass1907;
+using static MineDirectional;
+
+namespace WTTAN94
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static string[] GunIDs =  { "678fe4a4906c7bd23722c71f", "679a6a534f3d279c99b135b9" };
+
+        public static ConfigEntry<float> BurstROFMulti { get; set; }
+        public static ConfigEntry<float> BurstRecoilMulti { get; set; }
+        public static ConfigEntry<float> ShotResetDelay { get; set; }
+
+        public static bool IsAN94 { get; set; } = false;
+        public static bool IsFiring { get; set; } = false;
+        public static int ShotCount { get; set; } = 0;
+
+        private void Awake()
+        {
+            string settings = "Settings";
+            BurstROFMulti = Config.Bind<float>(settings, "Burst ROF Multi", 3f, new ConfigDescription("", new AcceptableValueRange<float>(1f, 4f), new ConfigurationManagerAttributes { Order = 1 }));
+            BurstRecoilMulti = Config.Bind<float>(settings, "Burst Rrecoil Multi", 0.5f, new ConfigDescription("", new AcceptableValueRange<float>(0.1f, 1f), new ConfigurationManagerAttributes { Order = 10}));
+            ShotResetDelay = Config.Bind<float>(settings, "Shot Reset Delay", 0.15f, new ConfigDescription("Time Delay After Firing To Determine If Firing Has Stopped.", new AcceptableValueRange<float>(0.01f, 2f), new ConfigurationManagerAttributes { Order = 15 }));
+
+            new UpdateWeaponVariablesPatch().Enable();
+            new ShootPatch().Enable();
+            new PlayerUpdatePatch().Enable();
+        }
+
+    }
+}
